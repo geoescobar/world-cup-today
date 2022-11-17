d71f63cf22b02c5fdfa818f865d9a1294f87c5a7
George Escobar <geo.escobar214@gmail.com>
2022-11-16 20:12:24 -0600

    package json update

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -6,25 +6,25 @@ function Games() {
 
   const [fixture, setFitxture] = useState("");
 
-  const fetchData = () => {
-    axios
-      .request(options)
-      .then(function (response) {
-        setFitxture(response.data.response[13].league.name);
-        console.log(response.data);
-        console.log(response.data.response[13].league.name);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
+  // const fetchData = () => {
+  //   axios
+  //     .request(options)
+  //     .then(function (response) {
+  //       setFitxture(response.data.response[13].league.name);
+  //       console.log(response.data);
+  //       console.log(response.data.response[13].league.name);
+  //     })
+  //     .catch(function (error) {
+  //       console.error(error);
+  //     });
+  // };
 
   return (
     <div>
       <h1>Test</h1>
-      <button className="btn btn-primary" onClick={fetchData}>
+      {/* <button className="btn btn-primary" onClick={fetchData}>
         Run
-      </button>
+      </button> */}
       {fixture}
     </div>
   );
